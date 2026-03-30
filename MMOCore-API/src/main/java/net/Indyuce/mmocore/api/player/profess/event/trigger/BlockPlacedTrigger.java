69f7a894f26fa0054b819dc312937a78749d09a3
Jules <phoenixdevtmc@gmail.com>
2026-03-30 11:52:45 +0200

    Fix MythicLib#362

--- MMOCore-API/src/main/java/net/Indyuce/mmocore/api/player/profess/event/trigger/BlockPlacedTrigger.java
+++ MMOCore-API/src/main/java/net/Indyuce/mmocore/api/player/profess/event/trigger/BlockPlacedTrigger.java
@@ -9,15 +9,21 @@ import org.bukkit.event.block.BlockPlaceEvent;
 @Deprecated
 public class BlockPlacedTrigger implements EventTriggerHandler {
 
-	@Override
-	public boolean handles(String event) {
-		return event.startsWith("place-block");
-	}
+    @Override
+    public boolean handles(String event) {
+        return event.startsWith("place-block");
+    }
 
-	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
-	public void a(BlockPlaceEvent event) {
-		PlayerData player = PlayerData.get(event.getPlayer());
-		if (player.getProfess().hasEventTriggers("place-block"))
-			player.getProfess().getEventTriggers("place-block").getTriggers().forEach(trigger -> trigger.apply(player));
-	}
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void a(BlockPlaceEvent event) {
+        PlayerData player;
+        try {
+            player = PlayerData.get(event.getPlayer());
+        } catch (NullPointerException exception) {
+            // Fixes https://gitlab.com/phoenix-dvpmt/mythiclib/-/work_items/362
+            return; // Player data not found
+        }
+        if (player.getProfess().hasEventTriggers("place-block"))
+            player.getProfess().getEventTriggers("place-block").getTriggers().forEach(trigger -> trigger.apply(player));
+    }
 }
